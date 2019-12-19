7bbad2cf75e1b6bada792616e70d76375ce0d17d
Julian Horner <julianhorner@web.de>
2019-12-19 10:39:34 +0100

    First attempt of implementing login page

--- src/main/java/de/rtuni/ms/apig/SecurityConfiguration.java
+++ src/main/java/de/rtuni/ms/apig/SecurityConfiguration.java
@@ -5,15 +5,11 @@
 
 package de.rtuni.ms.apig;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
@@ -38,19 +34,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             // make sure we use stateless session; session won't be used to store user's state.
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             // handle an authorized attempts 
-            .exceptionHandling().authenticationEntryPoint(
-                    (req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
+            //.exceptionHandling().authenticationEntryPoint(
+            //        (req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
             // Add a filter to validate the tokens with every request
             .addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig),
                     UsernamePasswordAuthenticationFilter.class)
             // authorization requests config
             .authorizeRequests()
             // allow all who are accessing "auth" service
-            .antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+            //.antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
             // must be an admin if trying to access secured page (authentication is also required)
-            .antMatchers("/securedPage/**").hasRole("ADMIN");
+            .antMatchers("/securedPage/**").hasRole("ADMIN").and()
+            .formLogin().loginPage("/login").permitAll().and()
+            .logout().permitAll();
     }
 
     //----------------------------------------------------------------------------------------------
