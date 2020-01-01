497f3057d0ae7de36c5207e5e0d4cc02143c51a4
Julian Horner <julianhorner@web.de>
2020-01-01 16:17:39 +0100

    Add login page and controller

---
+++ src/main/java/de/rtuni/ms/apig/LoginController.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 (C) by Julian Horner.
+ * All Rights Reserved.
+ */
+
+package de.rtuni.ms.apig;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Class that handles all kind of requests.
+ * 
+ * @author Julian
+ */
+@Controller
+public class LoginController {
+    //----------------------------------------------------------------------------------------------
+
+    /**
+     * Catch the request for the login page and returns the name of the corresponding template.
+     * 
+     * @return The name of the template
+     */
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    //----------------------------------------------------------------------------------------------
+}
